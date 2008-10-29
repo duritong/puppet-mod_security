14e2809c17910a45dcc0ce9060611cf545d0dedd
Marcel Haerry <haerry@puzzle.ch>
2008-10-29 21:56:29 +0100

    migrated module to fit for centos, added daily update routine for rules

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,47 +1,39 @@
 # modules/apache/manifests/modules/mod_security.pp
 # 2008 - admin(at)immerda.ch
+# Adapated by Puzzle ITC
 # License: GPLv3
 
 class mod_security {
-    case $operatingsystem {
-        gentoo: { include mod_security::gentoo }
-        default: { include mod_security::base }
-    }
+    include mod_security::base
 }
 
+# very centos stylish at the moment
 class mod_security::base {
-    #mod_unique_id is needed for mod_security
-    include mod_unique_id
-
-    package{mod_security:
+    package{'mod_security':
         ensure => installed,
         notify => Service[apache],
     }
 
-    file{custom_rules:
-        path => "/etc/apache2/modules.d/mod_security/Zcustom_rules/",
-        source => "puppet://$server/mod_security/custom_rules/",
-        recurse => true,
-        owner => root,
-        group => 0,
-        mode => 644,
+    file{'/etc/httpd/modsecurity.d/modsecurity_localrules.conf':
+        content => 'Include modsecurity.d/customrules/*.conf',
+        require => Package['mod_security'],
+        owner => root, group => 0; mode => 0644;
+    }
+
+    file{'/etc/httpd/modsecurity.d/customrules':
+        ensure => directory,
         require => Package[mod_security],
-        notify => Service[apache],
+        owner => root, group => 0, mode => 0755;
+    }
+
+    file{'/etc/cron.d/modsec.sh':
+        source => "puppet://$server/mod_security/cron/modesc.sh",
+        notify => Exec['update_modsec_rules'], 
+        require => File['/etc/httpd/modsecurity.d/customrules'],
+        owner => root, group => 0, mode => 0755;
+    }
+    exec{'update_modsec_rules':
+        command => '/etc/cron.d/modsec.sh',
+        refreshonly => true,
     }
 }
-
-class mod_security::gentoo inherits mod_security::base {
-    Package[mod_security]{
-        category => 'www-apache',
-    }
-
-    file{"/etc/apache2/modules.d/99_mod_security.conf":
-        source => "puppet://$server/mod_security/configs/gentoo/99_mod_security.conf",
-        owner => root,
-        group => 0,
-        mode => 644,
-        require => Package[mod_security],
-        notify => Service[apache],
-    }
-}
-
