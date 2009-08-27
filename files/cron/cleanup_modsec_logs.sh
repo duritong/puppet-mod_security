9152b5d45f0bb0328e2f70fad9b2cea73f15b3ce
Simon Josi <josi@puzzle.ch>
2009-08-27 10:10:04 +0200

    supress some error messages

--- files/cron/cleanup_modsec_logs.sh
+++ files/cron/cleanup_modsec_logs.sh
@@ -2,5 +2,5 @@
 
 # this cronjob cleans up any modsec logs
 # that might be in logs of vhosts
-test `ls /var/www/vhosts/* | grep logs | wc -l` -gt 0 \
+test `ls /var/www/vhosts/* 2>/dev/null | grep logs | wc -l` -gt 0 \
 && find /var/www/vhosts/*/logs/ -maxdepth 1 -type d -mtime +5 -exec rm -rf {} \;
