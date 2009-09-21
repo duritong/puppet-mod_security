375db206fb3907e60fcfb83319629840a2521546
Simon Josi <josi@puzzle.ch>
2009-09-21 11:18:48 +0200

    make cleanup_modsec_logs.sh sane

--- files/cron/cleanup_modsec_logs.sh
+++ files/cron/cleanup_modsec_logs.sh
@@ -2,5 +2,6 @@
 
 # this cronjob cleans up any modsec logs
 # that might be in logs of vhosts
-test `ls /var/www/vhosts/* 2>/dev/null | grep logs | wc -l` -gt 0 \
-&& find /var/www/vhosts/*/logs/ -maxdepth 1 -type d -mtime +5 -exec rm -rf {} \;
+ls -d /var/www/vhosts/*/logs 2>/dev/null | while read logdir; do
+  find $logdir -mindepth 1 -maxdepth 1 -type d -mtime +5 -print0 | xargs -0 rm -rf
+done
