7036b051544fc96b94172d6dd0d25d9cf70f51fd
mh <mh@immerda.ch>
2010-02-21 15:45:03 +0100

    add optional rules as well

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,2 +1,12 @@
 class mod_security::centos inherits mod_security::base {
+  file{'/etc/httpd/modsecurity.d/customrules/optional_rules.conf':
+    content => "Include modsecurity.d/optional_rules/*.conf\n",
+    ensure => $mod_security_optional_rules ? {
+                false => 'absent',
+                default => 'present'
+    },
+    require => Package['mod_security'],
+    notify => Service['apache'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
