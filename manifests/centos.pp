f1af54f98cf946631513eba9fdf8580555d236b4
mh <mh@immerda.ch>
2010-08-16 15:24:05 +0200

    finish itk_plus implementation

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,4 +1,9 @@
 class mod_security::centos inherits mod_security::base {
+  apache::config::global{'mod_security.conf':
+    source => "modules/mod_security/normal/${operatingsystem}/mod_security.conf",
+    require => Package['mod_security'],
+    notify => Service['apache'],
+  }
   file{'/etc/httpd/modsecurity.d/customrules/optional_rules.conf':
     content => "Include modsecurity.d/optional_rules/*.conf\n",
     ensure => $mod_security_optional_rules ? {
---
+++ manifests/itk_plus.pp
@@ -0,0 +1,5 @@
+class mod_security::itk_plus inherits mod_security::itk {
+  case $operatingsystem {
+    centos: { include mod_security::itk_plus::centos }
+  }
+}
