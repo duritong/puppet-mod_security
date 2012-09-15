c86f57e2e4421d2f198a7ae3298782c51dc467ce
mh <mh@immerda.ch>
2012-09-15 16:40:57 +0200

    update module to the new style how things are handled with the epel package

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -14,4 +14,26 @@ class mod_security::centos inherits mod_security::base {
     notify => Service['apache'],
     owner => root, group => 0, mode => 0644;
   }
+
+  package{'mod_security_crs': }
+  if mod_security::crs_ruleset {
+    Package['mod_security_crs']{
+      ensure => present,
+    }
+  } else {
+    Package['mod_security_crs']{
+      ensure => absent,
+    }
+  }
+
+  package{'mod_security_crs-extras': }
+  if mod_security::crs_extras_ruleset {
+    Package['mod_security_crs-extras']{
+      ensure => present,
+    }
+  } else {
+    Package['mod_security_crs-extras']{
+      ensure => absent,
+    }
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,10 @@
 
 class mod_security(
   $log_clean = hiera('mod_security_log_clean',false),
-  $asl_ruleset = hiera('mod_security_asl_ruleset',false)
+  $asl_ruleset = hiera('mod_security_asl_ruleset',false),
+  # so far they're only available on centos
+  $crs_ruleset = hiera('mod_security_crs_ruleset',false),
+  $crs_extras_ruleset = hiera('mod_security_crs_extras_ruleset',false)
 ) {
   case $::operatingsystem {
     centos: { include mod_security::centos }
