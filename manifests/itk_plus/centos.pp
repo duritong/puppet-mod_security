54d5a8bb5be7040a4ef309b155351f063bae1f56
mh <mh@immerda.ch>
2012-09-18 21:16:44 +0200

    adjust to new apache module style - make config tuneable by site module

--- manifests/itk_plus/centos.pp
+++ manifests/itk_plus/centos.pp
@@ -1,5 +1,9 @@
 class mod_security::itk_plus::centos inherits mod_security::centos {
   Apache::Config::Global['mod_security.conf']{
-    source => "modules/mod_security/itk_plus/${::operatingsystem}/mod_security.conf",
+    source  => [  "puppet:///modules/site_mod_security/itk_plus/${::fqdn}/mod_security.conf",
+                  "puppet:///modules/site_mod_security/itk_plus/${::domain}/mod_security.conf",
+                  'puppet:///modules/site_mod_security/itk_plus/mod_security.conf',
+                  "puppet:///modules/mod_security/itk_plus/${::operatingsystem}/mod_security.conf" ],
+
   }
 }
