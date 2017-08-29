fb46d10db8cb96259ceae74fe8debda2835ba320
mh <mh@immerda.ch>
2017-08-29 09:34:52 +0200

    an improved cleanup script

--- files/scripts/mod_security_logclean.sh
+++ files/scripts/mod_security_logclean.sh
@@ -3,6 +3,10 @@
 # clean up mod_security log directories that are within
 # a certain age
 [ -z $1 ] && echo "USAGE: $0 days_to_keep" && exit 1
-ls -d /var/www/vhosts/*/logs 2>/dev/null | while read logdir; do
-  find $logdir -mindepth 1 -maxdepth 1 -type d -mtime +${1} -print0 | xargs -0 rm -rf
+for vhost in /var/www/vhosts/*/logs; do
+  if ls $vhost/20* 1> /dev/null 2>&1; then
+    tmpwatch -q ${1}d $vhost/20*
+    find $vhost/20* -ignore_readdir_race -maxdepth 1 -mindepth 1 -type d -empty -delete
+    find $vhost/20* -ignore_readdir_race -maxdepth 0 -mindepth 0 -type d -empty -delete
+  fi
 done
