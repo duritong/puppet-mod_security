2bba642fd044984553a6e17a2a78df3bc5741445
mh <mh@immerda.ch>
2018-03-10 15:53:34 +0100

    another dir where tmp data might get stored

--- files/scripts/mod_security_logclean.sh
+++ files/scripts/mod_security_logclean.sh
@@ -3,7 +3,7 @@
 # clean up mod_security log directories that are within
 # a certain age
 [ -z $1 ] && echo "USAGE: $0 days_to_keep" && exit 1
-for vhost in /var/www/vhosts/*/logs; do
+for vhost in /var/www/vhosts/*/logs /var/lib/mod_security; do
   if ls $vhost/20* 1> /dev/null 2>&1; then
     tmpwatch -q ${1}d $vhost/20*
     find $vhost/20* -ignore_readdir_race -maxdepth 1 -mindepth 1 -type d -empty -delete
