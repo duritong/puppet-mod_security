85e08422fcabb81c9872ebb25a498a21662f8370
mh <mh@immerda.ch>
2018-03-25 16:36:40 +0200

    cleanup as unpriviledged user, as all mod_security logs are anyway written by apache & properly cleanup the global path

--- files/scripts/mod_security_logclean.sh
+++ files/scripts/mod_security_logclean.sh
@@ -3,10 +3,11 @@
 # clean up mod_security log directories that are within
 # a certain age
 [ -z $1 ] && echo "USAGE: $0 days_to_keep" && exit 1
-for vhost in /var/www/vhosts/*/logs /var/lib/mod_security; do
+for vhost in /var/www/vhosts/*/logs; do
   if ls $vhost/20* 1> /dev/null 2>&1; then
-    tmpwatch -q ${1}d $vhost/20*
-    find $vhost/20* -ignore_readdir_race -maxdepth 1 -mindepth 1 -type d -empty -delete
-    find $vhost/20* -ignore_readdir_race -maxdepth 0 -mindepth 0 -type d -empty -delete
+    su - apache -s /bin/bash -c "tmpwatch -q ${1}d $vhost/20*"
+    su - apache -s /bin/bash -c "find $vhost/20* -ignore_readdir_race -maxdepth 1 -mindepth 1 -type d -empty -delete"
+    su - apache -s /bin/bash -c "find $vhost/20* -ignore_readdir_race -maxdepth 0 -mindepth 0 -type d -empty -delete"
   fi
 done
+su - apache -s /bin/bash -c "tmpwatch -q ${1}d /var/lib/mod_security"
