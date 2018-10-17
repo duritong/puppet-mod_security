bf17dc91950fc9d6867e761bd54aa2b4832fb0c5
mh <mh@immerda.ch>
2018-10-17 07:43:35 +0200

    remove a rule that breaks multipart boundaries by default

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,6 +1,6 @@
 # centos specific things
 class mod_security::centos inherits mod_security::base {
-  if versioncmp($::operatingsystemmajrelease,'7') < 0 {
+  if versioncmp($acts['os']['release']['major'],'7') < 0 {
     apache::config::global{'mod_security.conf':
       source  => [  "puppet:///modules/site_mod_security/normal/${::fqdn}/mod_security.conf",
                     "puppet:///modules/site_mod_security/normal/${::domain}/mod_security.conf",
@@ -26,6 +26,16 @@ class mod_security::centos inherits mod_security::base {
         owner   => root,
         group   => 0,
         mode    => '0644';
+    }
+  }
+
+  # comment out an unwanted rule
+  # 200003 : broken multipart upload boundaries
+  if versioncmp($facts['os']['release']['major'],'6') > 0 {
+    Package<| title == 'mod_security' |> -> exec{'comment_out_rule_id_200003':
+      command => 'sed -i -e "s/ \(SecRule MULTIPART_UNMATCHED_BOUNDARY\)/ #\1/" -e "s/ \(\"id:\'200003\)/ #\1/" /etc/httpd/conf.d/mod_security.conf',
+      onlyif  => 'grep -qE " (SecRule MULTIPART_UNMATCHED_BOUNDARY|\"id:\'200003)" /etc/httpd/conf.d/mod_security.conf',
+      notify  => Service['apache'],
     }
   }
 }
